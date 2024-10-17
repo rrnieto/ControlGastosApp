3bdb933026eddd5d285431e71ec04b9928921439
Ruben R Nieto <ruben.nieto@eviden.com>
2024-10-17 15:33:03 +0200

    Historico de precios se incluye grafico

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Flask, Blueprint, render_template, request, redirect, url_for, Response
+import io
+import base64
 from .models import Ticket, Product, ProductTicket, PaymentType, Description
 from . import db
 from datetime import datetime
+from matplotlib.figure import Figure
+from matplotlib.dates import DateFormatter
+from matplotlib.ticker import MultipleLocator
 import json
 
 bp = Blueprint('main', __name__)
@@ -70,26 +75,63 @@ def add_expense():
 
 @bp.route('/product_history/<int:product_id>')
 def product_history(product_id):
-    # Obtener el producto y su historial de compras
+    # Obtener el producto y el historial de compras completo
     product = Product.query.get_or_404(product_id)
     price_history = ProductTicket.query.filter_by(product_id=product_id).order_by(ProductTicket.date.desc()).all()
+
+    # Filtrar el historial para mostrar solo cambios de precio
+    filtered_history = []
+    
+    for i in range(len(price_history)):
+        current_record = price_history[i]
+        next_record = price_history[i+1] if i+1 < len(price_history) else None
+
+        # Solo añadimos el registro si es el último con un precio constante antes de un cambio, o si es el último registro
+        if (next_record and current_record.price != next_record.price) or not next_record:
+            filtered_history.append(current_record)
 
     # Calcular el precio más bajo y el más alto
     lowest_price_record = ProductTicket.query.filter_by(product_id=product_id).order_by(ProductTicket.price.asc()).first()
     highest_price_record = ProductTicket.query.filter_by(product_id=product_id).order_by(ProductTicket.price.desc()).first()
     
-    # Usar el ticket_id del registro de historial más reciente para el enlace de retorno
-    last_ticket_id = price_history[0].ticket_id if price_history else None
+    # Definir last_ticket_id como el ticket del último elemento en filtered_history
+    last_ticket_id = filtered_history[-1].ticket_id if filtered_history else None
+
+    # Preparar datos para el gráfico, usando solo las fechas y precios de filtered_history
+    dates = [record.date for record in filtered_history]
+    prices = [record.price for record in filtered_history]
+
+    fig = Figure()
+    ax = fig.subplots()
+    ax.plot(dates, prices, marker='o', color='b')    
+    #ax.set_xlabel('Fecha')
+    #ax.set_ylabel('Precio (€)')
+    ax.grid(True)
+
+    # Formato de fecha personalizado en el eje X y establecer ticks específicos
+    date_format = DateFormatter("%d/%m/%y")
+    ax.xaxis.set_major_formatter(date_format)
+    ax.set_xticks(dates)  # Solo mostrar fechas con cambios de precio
+    fig.autofmt_xdate()  # Rotar las fechas para mejor visualización
+
+    # Ajustar el eje Y para incrementos de 0.05 euros
+    ax.yaxis.set_major_locator(MultipleLocator(0.05))
+
+    # Convertir gráfico a imagen para mostrar en el HTML
+    img = io.BytesIO()
+    fig.savefig(img, format='png')
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode()
 
     return render_template(
         'product_history.html',
         product=product,
-        price_history=price_history,
+        price_history=filtered_history,
         lowest_price_record=lowest_price_record,
         highest_price_record=highest_price_record,
-        last_ticket_id=last_ticket_id
+        last_ticket_id=last_ticket_id,
+        plot_url=plot_url
     )
-
 
 
 @bp.route('/')
@@ -99,7 +141,7 @@ def index():
 @bp.route('/view_tickets')
 def view_tickets():
     # Número de tickets a mostrar
-    ticket_count = 3
+    ticket_count = 10
     # Consultar los últimos tickets
     tickets = Ticket.query.order_by(Ticket.date.desc()).limit(ticket_count).all()
     return render_template('view_tickets.html', tickets=tickets)
